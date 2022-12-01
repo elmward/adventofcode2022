5fb5197ab8ce5cf6a7b916592eef8ed5782a7f8f
Mark Ashton <mark.ashton@provi.com>
2022-12-01 12:42:10 -0600

    Day 1 part 2

--- day1/part2.rb
+++ day1/part2.rb
@@ -1,7 +1,7 @@
 def main
   input = File.open('./input.txt').read
   elves = input.split("\n\n")
-  puts elves.map { |elf| elf.split("\n").map(&:to_i).sum }.max
+  puts elves.map { |elf| elf.split("\n").map(&:to_i).sum }.sort.reverse.take(3).sum
 end
 
 main if __FILE__ == $PROGRAM_NAME
